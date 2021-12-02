50d81ab04920caddce8115de517465ad654f6cd2
bcooperstl <coop999@gmail.com>
2021-12-01 21:38:21 -0600

    Added day 1 part 2 solution

--- src/solutions/aoc_day_1.cpp
+++ src/solutions/aoc_day_1.cpp
@@ -28,6 +28,16 @@ vector<long> AocDay1::read_input(string filename)
     return data;
 }
 
+vector<long> AocDay1::create_sliding_windows(vector<long> & measurements)
+{
+    vector<long> window_values;
+    for (int i=0; i<=measurements.size()-3; i++)
+    {
+        window_values.push_back(measurements[i] + measurements[i+1] + measurements[i+2]);
+    }
+    return window_values;
+}
+
 string AocDay1::part1(string filename, vector<string> extra_args)
 {
     vector<long> measurements = read_input(filename);
@@ -35,6 +45,23 @@ string AocDay1::part1(string filename, vector<string> extra_args)
     for (int i=1; i<measurements.size(); i++)
     {
         if (measurements[i] > measurements[i-1])
+        {
+            increase_counter++;
+        }
+    }
+    ostringstream out;
+    out << increase_counter;
+    return out.str();
+}
+
+string AocDay1::part2(string filename, vector<string> extra_args)
+{
+    vector<long> measurements = read_input(filename);
+    vector<long> window_values = create_sliding_windows(measurements);
+    long increase_counter = 0;
+    for (int i=1; i<window_values.size(); i++)
+    {
+        if (window_values[i] > window_values[i-1])
         {
             increase_counter++;
         }
