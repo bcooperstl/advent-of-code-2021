80964cc26b950f731e320ab6055f50142ccdbbc1
bcooperstl <coop999@gmail.com>
2021-12-28 22:06:18 -0600

    Added day 7 part 2

--- include/solutions/aoc_day_7.h
+++ include/solutions/aoc_day_7.h
@@ -9,11 +9,12 @@ class AocDay7 : public AocDay
 {
     private:
         vector<long> read_input(string filename);
+        long calculate_part2_fuel(long optimal, long starting);
     public:
         AocDay7();
         ~AocDay7();
         string part1(string filename, vector<string> extra_args);
-        //string part2(string filename, vector<string> extra_args);
+        string part2(string filename, vector<string> extra_args);
 };
 
 #endif
