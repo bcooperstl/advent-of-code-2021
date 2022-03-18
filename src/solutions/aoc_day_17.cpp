ba779d4853c14c2c370a768d770147e7eca5ef94
bcooperstl <coop999@gmail.com>
2022-03-17 21:23:42 -0500

    Added brute for solution for day 17 part 2

--- src/solutions/aoc_day_17.cpp
+++ src/solutions/aoc_day_17.cpp
@@ -131,6 +131,64 @@ bool AocDay17::check_y_velocity_is_hit(map<int, int> x_steps_to_velocities, int 
     return false;
 }
 
+void AocDay17::calculate_x_range_for_brute_force(int target_minimum_x, int target_maximum_x, int & range_minimum_x, int & range_maximum_x)
+{
+    range_maximum_x = target_maximum_x;
+    range_minimum_x = range_maximum_x;
+    cout << "Finding range for brute force with x between " << target_minimum_x << " and " << target_maximum_x << endl;
+    cout << "Setting maximum starting velocity to " << range_maximum_x << endl;
+    for (int start_x=target_maximum_x; start_x >= 1; start_x--)
+    {
+        int steps = 0;
+        int position = 0;
+        int velocity = start_x;
+        while (velocity > 0)
+        {
+            steps++;
+            position += velocity;
+            velocity --;
+            if (position >= target_minimum_x && position <= target_maximum_x)
+            {
+                cout << "Starting velocity " << start_x << " lands in range at " << position << " after " << steps << " steps" << endl;
+                if (start_x < range_minimum_x)
+                {
+                    range_minimum_x = start_x;
+                    cout << " Setting minimum starting velocity to " << range_minimum_x << endl;
+                }
+            }
+        }
+    }
+}
+
+bool AocDay17::does_hit_target(int start_velocity_x, int start_velocity_y, int target_minimum_x, int target_maximum_x, int target_minimum_y, int target_maximum_y)
+{
+    int velocity_x = start_velocity_x;
+    int velocity_y = start_velocity_y;
+    int position_x = 0;
+    int position_y = 0;
+    
+    while (position_x <= target_maximum_x)
+    {
+        position_x += velocity_x;
+        position_y += velocity_y;
+        velocity_y--;
+        if (velocity_x > 0)
+        {
+            velocity_x--;
+        }
+        if (position_x >= target_minimum_x && position_x <= target_maximum_x && position_y >= target_minimum_y && position_y <= target_maximum_y)
+        {
+            cout << "Hit with starting velocity " << start_velocity_x << "," << start_velocity_y << " by landing at " << position_x << "," << position_y << endl;
+            return true;
+        }
+        if ((position_y < target_minimum_y) && (velocity_y < 0)) // below the target and going down
+        {
+            return false;
+        }
+    }
+    return false;
+}
+
 string AocDay17::part1(string filename, vector<string> extra_args)
 {
     int target_minimum_x, target_maximum_x, target_minimum_y, target_maximum_y;
@@ -165,4 +223,42 @@ string AocDay17::part1(string filename, vector<string> extra_args)
     ostringstream out;
     out << max_peak_height;
     return out.str();
+}
+
+string AocDay17::part2(string filename, vector<string> extra_args)
+{
+    int target_minimum_x, target_maximum_x, target_minimum_y, target_maximum_y;
+    int range_minimum_x, range_maximum_x, range_minimum_y, range_maximum_y;
+    int range_minimum_steps = 1; // can get there in 1 step many ways
+    int range_maximum_steps;
+    parse_input(filename, target_minimum_x, target_maximum_x, target_minimum_y, target_maximum_y);
+    
+    cout << "Target area is from " << target_minimum_x << " <= x <= " << target_maximum_x
+         << " and " << target_minimum_y << " <= y <= " << target_maximum_y << endl;
+    
+    calculate_x_range_for_brute_force(target_minimum_x, target_maximum_x, range_minimum_x, range_maximum_x);
+    
+    cout << "For brute force, use X values between " << range_minimum_x << " and " << range_maximum_x << endl;
+    
+    range_minimum_y = target_minimum_y;
+    range_maximum_y = -1*target_minimum_y;
+    
+    cout << "For brute force, use Y values between " << range_minimum_y << " and " << range_maximum_y << endl;
+    
+    int num_found = 0;
+    for (int x=range_minimum_x; x<=range_maximum_x; x++)
+    {
+        for (int y=range_minimum_y; y<=range_maximum_y; y++)
+        {
+            if (does_hit_target(x, y, target_minimum_x, target_maximum_x, target_minimum_y, target_maximum_y))
+            {
+                num_found++;
+            }
+        }
+    }
+    
+    ostringstream out;
+    out << num_found;
+    return out.str();
+
 }
